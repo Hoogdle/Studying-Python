02aa9d4c676981027f587ea1fdc53669760f0b49
Taeyeong Kim <xodud7737@gmail.com>
2024-02-06 08:58:02 +0900

    Finished

--- 심화/Day1.py
+++ 심화/Day1.py
@@ -166,18 +166,148 @@
 #     inner()
 # outer()
 
-def outer():
-    name = "Kim"
-    print('outer',name) #otuer Kim
+# def outer():
+#     name = "Kim"
+#     print('outer',name) #otuer Kim
+
+#     def inner():
+#         nonlocal name #local에서 enclosed 변수(name)에 영향을 주겠다.
+#         name = name + 'Tae' 
+#         print('inner',name) #inner KimTae
+
+#     inner()
+#     print('outer after inner()',name) #outer after inner() KimTae
+# outer()
+
+
+
+## {closure - 클로져}
+# outer 함수 호출이 끝나면 inner 함수는 사라진다.
+# outer 함수 namespace가 사라지기 때문
+# outer가 끝나도 inner 함수를 기억하고 싶다면?
+# return inner
+# def outer():
+#     name = 'Kim'
+#     print('outer',name)
+#     def inner():
+#         name = 'Tae'
+#         print('inner',name)
+#     return inner
+# A = outer() #outer Kim
+# A() #inner Tae, #A가 return 값인 inner 함수를 받아옴
+
+# 만약 inner에서 nonlocal 키워드를 사용했다면?
+# def outer():
+#     name = 'Kim' 
+#     print('outer',name) #outer Kim
+#     def inner():
+#         nonlocal name
+#         name = name + 'Tae'
+#         print('inner',name) #inner Tae
+#     return inner
+# A = outer() 
+# A() 
+# outer Kim
+# inner KimTae
+# outer 함수 종료 후 name 변수가 삭제되어야 할텐데..?
+# A() #inner KimTaeTae
+# A() #inner KimTaeTaeTae
+# A() #inner KimTaeTaeTaeTae
+
+# https://youtu.be/5xXB93rX_X8?t=1938
+# outer 함수 종료, garbarge collector 가 처리, name 변수삭제
+# 이 때 name의 '상태정보'를 별도로 저장
+# A()를 호출할 때마다 name의 '상태정보' 수정 및 저장
+# A() 반복, '상태정보' 계속 업데이트
+
+# B = outer()
+# A와 B의 상태정보는 다르다
+# https://youtu.be/5xXB93rX_X8?t=2189
+# def outer():
+#     name = 'Kim' 
+#     print('outer',name) #outer Kim
+#     def inner():
+#         nonlocal name
+#         name = name + 'Tae'
+#         print('inner',name) #inner Tae
+#     return inner
+# A = outer() #outer Kim
+# B = outer() #outer Kim
+
+# A() #inner KimTae
+# A() #inner KimTaeTae
+# B() #inner KimTae
+
+# 심화
+# def outer(start):
+#     name = start
+#     print('outer',name)
+#     def inner():
+#         nonlocal name
+#         name = name + 'Tae'
+#         print('inner',name)
+#     return inner
+# A = outer('Kim') #outer Kim
+# B = outer('Kim') #outer Kim
+
+# A() #inner KimTae
+# A() #inner KimTaeTae
+# B() #inner KimTae
+
+
+
+## {Function Attributes - 함수 속성}
+# 함수.속성 == "함수 속성을 정할 수 있음"
+# def outer():
+#     name = "kim"
+#     print('outer',name)
+
+# outer.age = 30
+# outer.name = 'tae'
+# print(outer.age) #30
+# print(outer.name) #tae
+
+# attributes, 속성은 함수와 별개로 저장됨.
+# 함수의 name과 outer.name은 다름
+# def outer():
+#     name = "kim"
+#     print('outer',name)
+# #####################
+# Attributes:
+# outer.age = 30
+# outer.name = 'tae' 
+
+# closure 활용
+# def outer(start):
+#     name = start
+#     print('outer',name)
+
+#     def inner():
+#         inner.name = inner.name + 'tae'
+#         print('inner',inner.name)
+
+#     inner.name = name #함수 속성
+#     return inner
+# A = outer('Kim') #outer Kim
+# B = outer('Yeong') #outer Yeong
+# # 함수는 종료되었지만 상태정보에 inner.name의 정보를 저장
+# A() #inner Kimtae
+# B() #inner Yeongtae
+
+# List를 활용한 nonlocal
+# local에서 enclosed 변수 변경 => nonlocal 활용
+# List는 수정가능한 객체이므로 nonlocal을 사용하지 않아도 됨
+# 변수 name 자체, 참조하는 리스트는 변경되지 않음
+# 단지 리스트 첫 번째 index의 '내용'만 변경
+def outer(start):
+    name = [start]
+    print('outer',name)
 
     def inner():
-        nonlocal name #local에서 enclosed 변수(name)에 영향을 주겠다.
-        name = name + 'Tae' 
-        print('inner',name) #inner KimTae
-
-    inner()
-    print('outer after inner()',name) #outer after inner() KimTae
-outer()
-
-
-
+        name[0] = name[0] + 'tae'
+        print('inner',name[0])
+    return inner
+A = outer('Kim') #outer ['Kim']
+B = outer('Yeong') #outer ['Yeong']
+A() #inner Kimtae
+B() #inner Yeongtae
